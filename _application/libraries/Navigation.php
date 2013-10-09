adfb935dc660425d15eafb0efdb1615eaf7326de
Myo Kyaw Htun <myochauhtun@hotmail.com>
2013-10-09 16:41:16 +0700

    new page configuration

--- _application/libraries/Navigation.php
+++ _application/libraries/Navigation.php
@@ -13,9 +13,29 @@ class Navigation {
 
             foreach ($menu as $m){
                     ?>
+                <?php
+                    if(isset($m['submenu'])){
+                ?>
+                <li class="dropdown <?php echo $currentPage == $m['id'] ? ' active' : '';?>">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $m['title'];?></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/cms<?php echo $m['url'];?>" class="active"><?php echo $m['title'];?></a></li>
+                        <?php
+                            foreach ($m['submenu'] as $submenu){
+                        ?>
+                            <li <?php echo $currentPage == $submenu['id'] ? 'class="active"' : '';?>>
+                                <a href="/cms<?php echo $submenu['url'];?>"><?php echo $submenu['title'];?></a>
+                            </li>
+                        <?php
+                            }      
+                        ?>
+                    </ul>
+                </li>
+                <?php } else { ?>
                 <li <?php echo $currentPage == $m['id'] ? 'class="active"' : '';?>>
                     <a href="/cms<?php echo $m['url'];?>"><?php echo $m['title'];?></a>
                 </li>
+                <?php }?>
                 <?php
             }
     }
